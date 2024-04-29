1457c7199a2fd65e9628d2467094f65179f91581
Dragon <mohamedkhacha99@gmail.com>
2024-04-29 11:12:35 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// Home.js
+import React from 'react';
+import Header from '../components/Header';
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <h2>Home Page</h2>
+        </div>
+    );
+}
+
+export default Home;
